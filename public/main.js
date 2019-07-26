283e396806e8e386afe9e0964dd993ce44443164
Alpha Olomi <alphaolomi@gmail.com>
2019-07-26 14:47:21 +0300

    Initial Commit

---
+++ public/main.js
@@ -0,0 +1,93 @@
+const form = document.getElementById('vote-form');
+var event;
+
+form.addEventListener('submit', e=>{
+    
+    const choice = document.querySelector('input[name=os]:checked').value;
+    const data = {os: choice};
+
+    fetch('http://localhost:3000/poll',{
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }).then(res => res.json())
+    .catch(err => console.log(err));
+
+    e.preventDefault();
+});
+
+fetch("http://localhost:3000/poll")
+    .then(res => res.json())
+    .then(data => {
+        let votes = data.votes;
+        let totalVotes = votes.length;
+        document.querySelector('#chartTitle').textContent = `Total Votes: ${totalVotes}`;
+
+        let voteCounts = {
+            Windows: 0,
+            MacOS: 0,
+            Linux: 0,
+            Other: 0
+        };
+
+        voteCounts = votes.reduce((acc, vote) => (
+            (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc),
+            {}
+        );
+
+        let dataPoints = [
+            { label: 'Windows', y: voteCounts.Windows },
+            { label: 'MacOS', y: voteCounts.MacOS },
+            { label: 'Linux', y: voteCounts.Linux },
+            { label: 'Other', y: voteCounts.Other }
+        ];
+            
+        const chartContainer = document.querySelector('#chartContainer');
+        
+        if(chartContainer){
+
+            // Listen for the event.
+            document.addEventListener('votesAdded', function (e) { 
+                document.querySelector('#chartTitle').textContent = `Total Votes: ${e.detail.totalVotes}`;
+            });
+            
+            const chart = new CanvasJS.Chart('chartContainer', {
+                animationEnabled: true,
+                theme: 'theme1',
+                data:[
+                    {
+                        type: 'column',
+                        dataPoints: dataPoints
+                    }
+                ]
+            });
+            chart.render();
+        
+             // Enable pusher logging - don't include this in production
+             Pusher.logToConsole = true;
+        
+             var pusher = new Pusher('355bbcc1238451dd1d93', {
+               cluster: 'ap2',
+               encrypted: true
+             });
+         
+             var channel = pusher.subscribe('os-poll');
+
+             channel.bind('os-vote', function(data) {
+               dataPoints.forEach((point)=>{
+                   if(point.label==data.os)
+                   {
+                        point.y+=data.points;
+                        totalVotes+=data.points;
+                        event = new CustomEvent('votesAdded',{detail:{totalVotes:totalVotes}});
+                        // Dispatch the event.
+                        document.dispatchEvent(event);
+                   }
+               });
+               chart.render();
+             });
+        }
+
+});
