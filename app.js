80a3d86470d82dbf37ebbc0ff2d2165e3f534d20
Alpha Olomi <alphaolomi@gmail.com>
2019-08-22 18:39:29 +0300

    final changes

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'secret', resave: true, saveUninitialized: true}));
+// app.use(session({secret: 'secret', resave: true, saveUninitialized: true}));
 
 app.use(cors());
 
